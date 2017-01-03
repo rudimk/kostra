fb03f7fbc216baad007e4b262a0ad1a9c5ecfa19
Rudraksh MK <rudraksh@paperkite.in>
2017-01-03 19:00:19 +0530

    Cleaned up task definitions a bit.

--- Kostra/app/fabfile.py
+++ Kostra/app/fabfile.py
@@ -1,11 +1,14 @@
 from fabric.api import *
 from fabric.tasks import execute
 
+#----------------------------------------------------------
+## Update Ubuntu packages.
+
 def execute_update_packages(env, hosts):
-    print hosts
-    print env
     execute(update_packages, hosts=hosts, env=env)
 
 def update_packages(env):
     local("echo Updating package repositories...")
     sudo('apt-get update')
+
+#----------------------------------------------------------
